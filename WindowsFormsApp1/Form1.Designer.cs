aa328f28b67cdb3219d664e39732c00727242a3f
Andrea Sanri <andrea.santi@sinergest.com>
2025-07-02 17:50:04 +0200

    Aggiungere i file di progetto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnPlay = new System.Windows.Forms.Button();
+            this.BtnEasy = new System.Windows.Forms.Button();
+            this.BtnMedium = new System.Windows.Forms.Button();
+            this.BtnHard = new System.Windows.Forms.Button();
+            this.LblTitolo = new System.Windows.Forms.Label();
+            this.TxBNumero = new System.Windows.Forms.TextBox();
+            this.BtnVerificaNumero = new System.Windows.Forms.Button();
+            this.LblMaxMin = new System.Windows.Forms.Label();
+            this.LblTentativi = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // BtnPlay
+            // 
+            this.BtnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlay.Location = new System.Drawing.Point(35, 122);
+            this.BtnPlay.Name = "BtnPlay";
+            this.BtnPlay.Size = new System.Drawing.Size(134, 48);
+            this.BtnPlay.TabIndex = 0;
+            this.BtnPlay.Text = "Play";
+            this.BtnPlay.UseVisualStyleBackColor = true;
+            this.BtnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // BtnEasy
+            // 
+            this.BtnEasy.Location = new System.Drawing.Point(35, 179);
+            this.BtnEasy.Name = "BtnEasy";
+            this.BtnEasy.Size = new System.Drawing.Size(85, 41);
+            this.BtnEasy.TabIndex = 1;
+            this.BtnEasy.Text = "Facile";
+            this.BtnEasy.UseVisualStyleBackColor = true;
+            this.BtnEasy.Visible = false;
+            this.BtnEasy.Click += new System.EventHandler(this.BtnEasy_Click);
+            // 
+            // BtnMedium
+            // 
+            this.BtnMedium.Location = new System.Drawing.Point(35, 226);
+            this.BtnMedium.Name = "BtnMedium";
+            this.BtnMedium.Size = new System.Drawing.Size(85, 41);
+            this.BtnMedium.TabIndex = 2;
+            this.BtnMedium.Text = "Medio";
+            this.BtnMedium.UseVisualStyleBackColor = true;
+            this.BtnMedium.Visible = false;
+            this.BtnMedium.Click += new System.EventHandler(this.BtnMedium_Click);
+            // 
+            // BtnHard
+            // 
+            this.BtnHard.Location = new System.Drawing.Point(35, 273);
+            this.BtnHard.Name = "BtnHard";
+            this.BtnHard.Size = new System.Drawing.Size(85, 41);
+            this.BtnHard.TabIndex = 3;
+            this.BtnHard.Text = "Difficile";
+            this.BtnHard.UseVisualStyleBackColor = true;
+            this.BtnHard.Visible = false;
+            this.BtnHard.Click += new System.EventHandler(this.BtnHard_Click);
+            // 
+            // LblTitolo
+            // 
+            this.LblTitolo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitolo.Location = new System.Drawing.Point(228, 50);
+            this.LblTitolo.Name = "LblTitolo";
+            this.LblTitolo.Size = new System.Drawing.Size(300, 41);
+            this.LblTitolo.TabIndex = 4;
+            this.LblTitolo.Text = "Indovina il numero";
+            this.LblTitolo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxBNumero
+            // 
+            this.TxBNumero.Location = new System.Drawing.Point(323, 161);
+            this.TxBNumero.Name = "TxBNumero";
+            this.TxBNumero.Size = new System.Drawing.Size(100, 20);
+            this.TxBNumero.TabIndex = 5;
+            this.TxBNumero.Visible = false;
+            // 
+            // BtnVerificaNumero
+            // 
+            this.BtnVerificaNumero.Location = new System.Drawing.Point(334, 187);
+            this.BtnVerificaNumero.Name = "BtnVerificaNumero";
+            this.BtnVerificaNumero.Size = new System.Drawing.Size(75, 23);
+            this.BtnVerificaNumero.TabIndex = 6;
+            this.BtnVerificaNumero.Text = "Verifica";
+            this.BtnVerificaNumero.UseVisualStyleBackColor = true;
+            this.BtnVerificaNumero.Visible = false;
+            this.BtnVerificaNumero.Click += new System.EventHandler(this.BtnVerificaNumero_Click);
+            // 
+            // LblMaxMin
+            // 
+            this.LblMaxMin.Location = new System.Drawing.Point(377, 242);
+            this.LblMaxMin.Name = "LblMaxMin";
+            this.LblMaxMin.Size = new System.Drawing.Size(123, 42);
+            this.LblMaxMin.TabIndex = 7;
+            this.LblMaxMin.Visible = false;
+            // 
+            // LblTentativi
+            // 
+            this.LblTentativi.Location = new System.Drawing.Point(248, 242);
+            this.LblTentativi.Name = "LblTentativi";
+            this.LblTentativi.Size = new System.Drawing.Size(123, 44);
+            this.LblTentativi.TabIndex = 8;
+            this.LblTentativi.Visible = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblTentativi);
+            this.Controls.Add(this.LblMaxMin);
+            this.Controls.Add(this.BtnVerificaNumero);
+            this.Controls.Add(this.TxBNumero);
+            this.Controls.Add(this.LblTitolo);
+            this.Controls.Add(this.BtnHard);
+            this.Controls.Add(this.BtnMedium);
+            this.Controls.Add(this.BtnEasy);
+            this.Controls.Add(this.BtnPlay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPlay;
+        private System.Windows.Forms.Button BtnEasy;
+        private System.Windows.Forms.Button BtnMedium;
+        private System.Windows.Forms.Button BtnHard;
+        private System.Windows.Forms.Label LblTitolo;
+        private System.Windows.Forms.TextBox TxBNumero;
+        private System.Windows.Forms.Button BtnVerificaNumero;
+        private System.Windows.Forms.Label LblMaxMin;
+        private System.Windows.Forms.Label LblTentativi;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        const int min = 1;
+        const int maxE = 50;
+        const int maxM = 100;
+        const int maxD = 500;
+        int secondiPassati = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            BtnPlay.Visible = false;
+            BtnEasy.Visible = true;
+            BtnMedium.Visible = true;
+            BtnHard.Visible = true;
+        }
+
+        private void BtnEasy_Click(object sender, EventArgs e)
+        {
+            BtnEasy.Visible = false;
+            BtnMedium.Visible = false;
+            BtnHard.Visible = false;
+            TxBNumero.Visible = true;
+            BtnVerificaNumero.Visible = true;
+            LblMaxMin.Visible = true;
+            LblTentativi.Visible = true;
+            Timer.Start();
+            global.Tentativi = 10;
+            global.TentativiMassimi = global.Tentativi;
+            global.TempoMassimo = 120;
+            global.Numero = r.Next(min, maxE + 1);
+            global.max = maxE;
+            TxBNumero.Focus();
+        }
+
+        private void BtnMedium_Click(object sender, EventArgs e)
+        {
+            BtnEasy.Visible = false;
+            BtnMedium.Visible = false;
+            BtnHard.Visible = false;
+            TxBNumero.Visible = true;
+            BtnVerificaNumero.Visible = true;
+            LblMaxMin.Visible = true;
+            LblTentativi.Visible = true;
+            Timer.Start();
+            global.Tentativi = 7;
+            global.TentativiMassimi = global.Tentativi;
+            global.TempoMassimo = 90;
+            global.Numero = r.Next(min, maxM + 1);
+            global.max = maxM;
+            TxBNumero.Focus();
+        }
+
+        private void BtnHard_Click(object sender, EventArgs e)
+        {
+            BtnEasy.Visible = false;
+            BtnMedium.Visible = false;
+            BtnHard.Visible = false;
+            TxBNumero.Visible = true;
+            BtnVerificaNumero.Visible = true;
+            LblMaxMin.Visible = true;
+            LblTentativi.Visible = true;
+            Timer.Start();
+            global.Tentativi = 5;
+            global.TentativiMassimi = global.Tentativi;
+            global.TempoMassimo = 60;
+            global.Numero = r.Next(min, maxD + 1);
+            global.max = maxD;
+            TxBNumero.Focus();
+        }
+
+        private void BtnVerificaNumero_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(TxBNumero.Text, out int o) && o > 0 && o < global.max)
+            {
+                if (o == global.Numero)
+                {
+                    DialogResult risultato = MessageBox.Show("Hai vinto!!!" + "\n" + "Vuoi rigiocare?", "Vittoria", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (risultato == DialogResult.Yes)
+                    {
+                        BtnEasy.Visible = true;
+                        BtnMedium.Visible = true;
+                        BtnHard.Visible = true;
+                        TxBNumero.Visible = false;
+                        BtnVerificaNumero.Visible = false;
+                        LblMaxMin.Text = "";
+                        LblMaxMin.Visible = false;
+                        LblTentativi.Visible = false;
+                        Timer.Stop();
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+                else if (o > global.Numero)
+                {
+                    LblMaxMin.Text = "Il numero è minore rispetto al tuo";
+                    global.Tentativi--;
+                    LblTentativi.Text = "Tentativi rimanenti: " + global.Tentativi.ToString();
+                    TxBNumero.Clear();
+                }
+                else
+                {
+                    LblMaxMin.Text = "Il numero è maggiore rispetto al tuo";
+                    global.Tentativi--;
+                    LblTentativi.Text = "Tentativi rimanenti: " + global.Tentativi.ToString();
+                    TxBNumero.Clear();
+                }
+                if (global.Tentativi == 0)
+                {
+                    DialogResult risultato = MessageBox.Show($"Hai perso per aver terminato i {global.TentativiMassimi} tentativi" + "\n" + "Vuoi rigiocare?", "Sconfitta", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (risultato == DialogResult.Yes)
+                    {
+                        BtnEasy.Visible = true;
+                        BtnMedium.Visible = true;
+                        BtnHard.Visible = true;
+                        TxBNumero.Visible = false;
+                        BtnVerificaNumero.Visible = false;
+                        LblMaxMin.Text = "";
+                        LblMaxMin.Visible = false;
+                        LblTentativi.Visible = false;
+                        Timer.Stop();
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Riprova mettendo un numero valido");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AcceptButton = BtnVerificaNumero;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            secondiPassati++;
+            if (global.TempoMassimo == 120)
+            {
+                if (secondiPassati == 60)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == 90)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == global.TempoMassimo)
+                {
+                    DialogResult risultato = MessageBox.Show($"Hai perso perchè è scaduto il tempo" + "\n" + "Vuoi rigiocare?", "Sconfitta", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (risultato == DialogResult.Yes)
+                    {
+                        BtnEasy.Visible = true;
+                        BtnMedium.Visible = true;
+                        BtnHard.Visible = true;
+                        TxBNumero.Visible = false;
+                        BtnVerificaNumero.Visible = false;
+                        LblMaxMin.Text = "";
+                        LblMaxMin.Visible = false;
+                        LblTentativi.Visible = false;
+                        Timer.Stop();
+                        secondiPassati = 0;
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+            }
+            else if (global.TempoMassimo == 90)
+            {
+                if (secondiPassati == 30)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == 60)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == global.TempoMassimo)
+                {
+                    DialogResult risultato = MessageBox.Show($"Hai perso perchè è scaduto il tempo" + "\n" + "Vuoi rigiocare?", "Sconfitta", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (risultato == DialogResult.Yes)
+                    {
+                        BtnEasy.Visible = true;
+                        BtnMedium.Visible = true;
+                        BtnHard.Visible = true;
+                        TxBNumero.Visible = false;
+                        BtnVerificaNumero.Visible = false;
+                        LblMaxMin.Text = "";
+                        LblMaxMin.Visible = false;
+                        LblTentativi.Visible = false;
+                        Timer.Stop();
+                        secondiPassati = 0;
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+            }
+            else if (global.TempoMassimo == 60)
+            {
+                if (secondiPassati == 15)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == 30)
+                {
+                    MessageBox.Show($"passati {secondiPassati} secondi");
+                }
+                else if (secondiPassati == global.TempoMassimo)
+                {
+                    DialogResult risultato = MessageBox.Show($"Hai perso perchè è scaduto il tempo" + "\n" + "Vuoi rigiocare?", "Sconfitta", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (risultato == DialogResult.Yes)
+                    {
+                        BtnEasy.Visible = true;
+                        BtnMedium.Visible = true;
+                        BtnHard.Visible = true;
+                        TxBNumero.Visible = false;
+                        BtnVerificaNumero.Visible = false;
+                        LblMaxMin.Text = "";
+                        LblMaxMin.Visible = false;
+                        LblTentativi.Visible = false;
+                        Timer.Stop();
+                        secondiPassati = 0;
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+            }
+
+
+        }
+    }
+    public static class global
+    {
+        public static int Tentativi;
+        public static int TentativiMassimi;
+        public static int TempoMassimo;
+        public static int Numero;
+        public static int max;
+    }
+}
